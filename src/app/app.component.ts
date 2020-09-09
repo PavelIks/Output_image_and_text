0906f485c233defca73165433872c6aef8e80869
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-09-09 14:20:35 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component} from '@angular/core';
+ 
+@Component({
+    selector: 'my-app',
+	template: 
+	`<h2>
+		If you think your teacher is tough, wait till you get a boss<br>
+		Life is not fair — get used to it!<br>
+		Success is a lousy teacher. It seduces smart people into<br>
+		thinking they can’t lose<br>
+		Be nice to nerds. Chances are you’ll end up working for one<br>
+	</h2>
+	
+	<img src="https://cdni.rt.com/inotv/s/content/v/8/8/1058100_1_gates_big.JPG" alt="...">
+
+	<p>William Henry Gates III (born October 28, 1955) is an American business magnate, software developer, investor, and philanthropist. He is best known as the co-founder of Microsoft Corporation.[2][3] During his career at Microsoft, Gates held the positions of chairman, chief executive officer (CEO), president and chief software architect, while also being the largest individual shareholder until May 2014. He is one of the best-known entrepreneurs and pioneers of the microcomputer revolution of the 1970s and 1980s.</p>
+	`
+})
+export class AppComponent { }
